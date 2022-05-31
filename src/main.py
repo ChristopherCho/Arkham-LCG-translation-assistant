3d6caeca598993aa86dd6d88690bf9ae6c529dfe
Jungho Christopher Cho <wjdgh6655@gmail.com>
2022-05-31 12:33:38 +0900

    feat: add arguments

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from tkinter import Tk
+from argparse import ArgumentParser
 
 from gui import GUIApp
 
 if __name__=="__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--chromedriver", type=str, default='data/chromedriver_windows', help="Path to chromedriver")
+    parser.add_argument("--image_scale", type=float, default=2.5, help="Ratio to scale up/down image from original (300 * 420)")
+    parser.add_argument("--save_original_size", action="store_true", help="Whether to save image in original size (300 * 420)")
+
+    args = parser.parse_args()
+
     root = Tk()
     root.title("Image change")
-    app = GUIApp(root)
+    app = GUIApp(root, args)
     root.mainloop()
